66655d23f490e0a6949dd6795f7ac0eed489567a
LINKE-3 <71629466+LINKE-3@users.noreply.github.com>
2021-02-22 11:37:10 +0100

    Update view.py

--- view.py
+++ view.py
@@ -39,7 +39,7 @@ class View:
     
    
     def display_wall(self):
-        Wall = pygame.image.load("C:\\Users\\alain\\Documents\\Open Classroom\\P3_Linke_Marin\\wall.png")
+        Wall = pygame.image.load("ressource\\wall.png")
         Wall.convert()
         Wall = pygame.transform.scale(Wall, [Wall.get_width() - 25, Wall.get_height() - 22])
         map = read_map()
@@ -52,7 +52,7 @@ class View:
         #wall read load and position
     
     def display_floor(self):
-        Floor = pygame.image.load("C:\\Users\\alain\\Documents\\Open Classroom\\P3_Linke_Marin\\floor.png")
+        Floor = pygame.image.load("ressource\\floor.png")
         Floor.convert()
         Floor = pygame.transform.scale(Floor, [Floor.get_width() - 31, Floor.get_height() - 32])
         map = read_map()
@@ -70,7 +70,7 @@ class View:
 
   
     def display_hero(self,mcgyver):
-        MacGyver = pygame.image.load("C:\\Users\\alain\\Documents\\Open Classroom\\P3_Linke_Marin\\MacGyver.png")
+        MacGyver = pygame.image.load("ressource\\MacGyver.png")
         #load image
         MacGyver.convert()
         #convert l'image
@@ -84,13 +84,13 @@ class View:
     def display_items(self):
         place=map_place()
         lieu = get_random_place(place)
-        needle = pygame.image.load("C:\\Users\\alain\\Documents\\Open Classroom\\P3_Linke_Marin\\1.png")
+        needle = pygame.image.load("ressource\\1.png")
         needle = pygame.transform.scale(needle, [needle.get_width() - 500, needle.get_height() - 650])
         needle.convert()
-        Ether = pygame.image.load("C:\\Users\\alain\\Documents\\Open Classroom\\P3_Linke_Marin\\2.png")
+        Ether = pygame.image.load("ressource\\2.png")
         Ether = pygame.transform.scale(Ether, [Ether.get_width() - 175, Ether.get_height() - 175])
         Ether.convert()
-        syringe = pygame.image.load("C:\\Users\\alain\\Documents\\Open Classroom\\P3_Linke_Marin\\0.png")
+        syringe = pygame.image.load("ressource\\0.png")
         syringe = pygame.transform.scale(syringe, [syringe.get_width() - 40, syringe.get_height() - 40])
         syringe.convert()
         self.window_surface.blit(needle, lieu[0])
@@ -101,10 +101,10 @@ class View:
         #items read load and position
         
     def arrive(self):
-        black = pygame.image.load("C:\\Users\\alain\\Documents\\Open Classroom\\P3_Linke_Marin\\black.png")
+        black = pygame.image.load("ressource\\black.png")
         black = pygame.transform.scale(black, [black.get_width() - 10, black.get_height() - 10])
         black.convert()
-        guardian = pygame.image.load("C:\\Users\\alain\\Documents\\Open Classroom\\P3_Linke_Marin\\guardian.png")
+        guardian = pygame.image.load("ressource\\guardian.png")
         guardian.convert()
         self.window_surface.blit(black, [13*X_SIZE, 1*Y_SIZE])
         self.window_surface.blit(guardian, [13*X_SIZE, 1*Y_SIZE])
@@ -112,7 +112,7 @@ class View:
         #arrive read load and position
         
     def display_remove(self, x ,y):
-        Floor = pygame.image.load("C:\\Users\\alain\\Documents\\Open Classroom\\P3_Linke_Marin\\floor.png")
+        Floor = pygame.image.load("ressource\\floor.png")
         Floor.convert()
         Floor = pygame.transform.scale(Floor, [Floor.get_width() - 31, Floor.get_height() - 32])
         map = read_map()
